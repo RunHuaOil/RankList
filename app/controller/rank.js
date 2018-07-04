354d95c66721dc77a4963b993dfe8466bda64e90
RunHuaOil <540060645@qq.com>
2018-07-04 16:45:26 +0800

    增加两个删除排行榜的管理接口

--- app/controller/rank.js
+++ app/controller/rank.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
-let key = 'lizhaoji';
-let setKey = 'rank:score';
+const key = 'lizhaoji';
+const setKey = 'rank:score';
 
 async function addScore(ctx) {
     const schema = Joi.object().keys({
@@ -11,12 +11,12 @@ async function addScore(ctx) {
     });
 
     let validateObj = ctx.request.body || {};
-    const { error, value } = Joi.validate(validateObj, schema);
-    if (error) return ctx.response.body = { code: 0, msg: error.toString() };
-    if (value.key !== key) return ctx.response.body = { code: -1, msg: 'key error' };
+    const {error, value} = Joi.validate(validateObj, schema);
+    if (error) return ctx.response.body = {code: 0, msg: error.toString()};
+    if (value.key !== key) return ctx.response.body = {code: -1, msg: 'key error'};
 
     let index = await ctx.app.redis.zrank(setKey, value.name);
-    if (index !== null) return ctx.response.body = { code: -2, msg: 'name existed' };
+    if (index !== null) return ctx.response.body = {code: -2, msg: 'name existed'};
 
     let [result, rankList] = await Promise.all([
         ctx.app.redis.zadd(setKey, value.score, value.name),
@@ -25,16 +25,16 @@ async function addScore(ctx) {
 
     if (rankList.length !== 0) ctx.app.redis.zrem(setKey, rankList);
 
-    ctx.response.body = { code: 1, msg: 'success to add score' }
+    ctx.response.body = {code: 1, msg: 'success to add score'}
 }
 
 async function getRank(ctx) {
     const schema = Joi.object().keys({
         key: Joi.string().token().min(1).required(),
     });
-    const { error, value } = Joi.validate(ctx.query, schema);
-    if (error) return ctx.response.body = { code: 0, msg: error.toString() };
-    if (value.key !== key) return ctx.response.body = { code: -1, msg: 'key error' };
+    const {error, value} = Joi.validate(ctx.query, schema);
+    if (error) return ctx.response.body = {code: 0, msg: error.toString()};
+    if (value.key !== key) return ctx.response.body = {code: -1, msg: 'key error'};
 
     let rankList = await ctx.app.redis.zrevrange(setKey, 0, 99, 'WITHSCORES');
 
@@ -44,11 +44,42 @@ async function getRank(ctx) {
         let score = rankList.shift();
         dataList.push({name, score});
     }
-    ctx.response.body = { code: 1, msg: 'success to get rankList', data: dataList }
+    ctx.response.body = {code: 1, msg: 'success to get rankList', data: dataList}
+}
+
+
+async function delScoreByName(ctx) {
+    const schema = Joi.object().keys({
+        key: Joi.string().token().min(1).required(),
+        name: Joi.string().min(1).max(16).required(),
+    });
+    const {error, value} = Joi.validate(ctx.query, schema);
+    if (error) return ctx.response.body = {code: 0, msg: error.toString()};
+    if (value.key !== key) return ctx.response.body = {code: -1, msg: 'key error'};
+
+    let res = await ctx.app.redis.zrem(setKey, value.name);
+    if (!res) return ctx.response.body = {code: -5, msg: '删除失败,名字或不存在'};
+    return ctx.response.body = {code: 1, msg: `success to del ${value.name} score`}
+}
+
+
+async function delRankList(ctx) {
+    const schema = Joi.object().keys({
+        key: Joi.string().token().min(1).required(),
+    });
+    const {error, value} = Joi.validate(ctx.query, schema);
+    if (error) return ctx.response.body = {code: 0, msg: error.toString()};
+    if (value.key !== key) return ctx.response.body = {code: -1, msg: 'key error'};
+
+    let res = await ctx.app.redis.del(setKey);
+    if (!res) return ctx.response.body = {code: -5, msg: '删除失败'};
+    return ctx.response.body = {code: 1, msg: `success to del rank`}
 }
 
 
 module.exports = {
     addScore,
-    getRank
+    getRank,
+    delScoreByName,
+    delRankList
 };
--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ let router = new Router();
  
 router.get('/api/rank', rank.getRank);
 router.post('/api/score', rank.addScore);
+router.delete('/api/admin/score', rank.delScoreByName);
+router.delete('/api/admin/rank', rank.delRankList);
 
 router.post('/api/admin/version', version.updateVersion);
 router.delete('/api/admin/version', version.delVersion);
